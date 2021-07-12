b26d27793edfdb05fa0a3630448ce212b78dc37b
alex2116 <reingeld116@gmail.com>
2021-07-12 18:38:53 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 
 const exphbs = require('express-handlebars')
 
+const bodyParser = require('body-parser')
+
 const Todo = require('./models/todo')
 
 const app = express()
@@ -22,12 +24,34 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout:'main', extname:'.hbs'})) //副檔名要幹嘛？
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   //拿到全部的todo資料
   Todo.find() //沒有傳入任何參數，所以會撈出整份資料
   .lean()
-  .then(todos => res.render('index', {todos}))
+  .then(todos => res.render('index', {todos})) //這邊也有return 只是省略了
   .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req,res) => {
+  return res.render('new') //為什這邊要return（已解）
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
+  
+  return Todo.create({ name }) 
+  .then(() => res.redirect('/'))
+  .catch(error => console.log(error))
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('detail', {todo}))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
