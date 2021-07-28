51ba9733d633bf4ef4f8e0e280aab81f5bed6367
alex2116 <reingeld116@gmail.com>
2021-07-28 22:48:23 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()       
 const home = require('./modules/home') //載入home.js
 const todos = require('./modules/todos') //載入todo.js
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)
 
 module.exports = router
 
