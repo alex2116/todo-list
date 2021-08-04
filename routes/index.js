81a9a9b0e824ab6f313caffc06337e601d40259e
alex2116 <reingeld116@gmail.com>
2021-08-04 19:15:07 +0800

    feat: add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ const router = express.Router()
 const home = require('./modules/home') //載入home.js
 const todos = require('./modules/todos') //載入todo.js
 const users = require('./modules/users')
-
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 
 router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
